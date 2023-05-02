40f85730b59391fbc25cbc6af518bbbac775801a
gmoraes <112905369+guilhermemoraessiqueira@users.noreply.github.com>
2023-05-02 17:15:44 -0300

    feat: aplicação spring boot c/ dependencia Spring Web, Lombok, Spring Boot DevTools e PostgreSQL

---
+++ projetoHealthAndCareBackEnd/src/main/java/com/healthandcare/projeto/projetoHealthAndCare/ProjetoHealthAndCareApplication.java
@@ -0,0 +1,13 @@
+package com.healthandcare.projeto.projetoHealthAndCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHealthAndCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoHealthAndCareApplication.class, args);
+	}
+
+}
---
+++ projetoHealthAndCareBackEnd/src/test/java/com/healthandcare/projeto/projetoHealthAndCare/ProjetoHealthAndCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthandcare.projeto.projetoHealthAndCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHealthAndCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
